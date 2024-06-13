ee48dbc8cf0710ed8952ca16d2046659d8c64399
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2024-06-13 12:06:11 +0200

    Initial commit

---
+++ tuuziane/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lw2e7rebhjw%t#dz_hp)ojguol!zj4yw9rp-3h-512w_2-$$p)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
