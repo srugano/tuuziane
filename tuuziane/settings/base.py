52593788462b90fc0284eca5ad28d5b79f547b27
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2024-06-14 19:28:39 +0200

    Added pre-commit

--- tuuziane/settings/base.py
+++ tuuziane/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 from oscar.defaults import *
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- tuuziane/urls.py
+++ tuuziane/urls.py
@@ -1,18 +1,16 @@
-from django.conf import settings
-from django.urls import include, path
-from django.contrib import admin
-
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
 from django.apps import apps
-
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    path("", include(apps.get_app_config("oscar").urls[0]))
+    path("", include(apps.get_app_config("oscar").urls[0])),
 ]
 
 
