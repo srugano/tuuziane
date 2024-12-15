6e8e1b85ffaa4de9d24b399fdcd5dab76c887508
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2024-12-15 23:22:14 +0100

    Formated htmls

--- tuuziane/settings/base.py
+++ tuuziane/settings/base.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     "oscar.apps.checkout.apps.CheckoutConfig",
     "oscar.apps.address.apps.AddressConfig",
     "oscar.apps.shipping.apps.ShippingConfig",
-    "oscar.apps.catalogue.apps.CatalogueConfig",
+    "apps.catalogue.apps.CatalogueConfig",
     "oscar.apps.catalogue.reviews.apps.CatalogueReviewsConfig",
     "oscar.apps.communication.apps.CommunicationConfig",
     "oscar.apps.partner.apps.PartnerConfig",
