878f90e191332692ccb31f8eac5169ed728b5ca1
landryPrct <landryhabonimana@outlook.com>
2025-01-31 17:52:28 +0200

    Update Home.vue

--- tuuziane_front/src/components/Home.vue
+++ tuuziane_front/src/components/Home.vue
@@ -1,7 +1,51 @@
-<script setup>
-
-</script>
-
+<script setup></script>
+<script setup lang="ts"></script>
 <template>
+  <section-1>
+    <main></main>
 
+    <sub-section>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div
+            class="col-10"
+            style="backdrop-filter: blur(5px); background-color: rgba(0, 0, 0, 0.101);border-radius: 20px"
+          >
+
+          <!-- search  -->
+            <h5 class="text-center"><b>"Recherchez. Trouvez. Profitez."</b></h5>
+
+            <div class="container">
+              <div class="row rounded-pill justify-content-center">
+                <div class="col-auto">
+                  <div class=" bg-white rounded-pill" style="display: flex; padding: 2px">
+                    <div><input class="form-control rounded-pill" placeholder="Recherche ..." type="text" /></div>
+                    <div>
+                      <button class="rounded-pill btn bg-orange " style="background-color: var(--vt-c-orage-1)" type="button">
+                        <i class="bx bx-search-alt-2"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+<!-- category list  -->
+
+
+
+<div style="display: flex" align="center">
+<div class="mx-3"> <img class="img-fluid  cat rounded-pill" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOD0abk6GHTIRcOLItB1UnxDP8NtyACkMCA&s" alt="">
+</div>
+<div class="mx-3 "><img class="img-fluid  cat rounded-pill"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOD0abk6GHTIRcOLItB1UnxDP8NtyACkMCA&s" alt=""></div>
+<div class="mx-3 "><img class="img-fluid  cat rounded-pill"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOD0abk6GHTIRcOLItB1UnxDP8NtyACkMCA&s" alt=""></div>
+<div class="mx-3 "><img class="img-fluid  cat rounded-pill"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgOD0abk6GHTIRcOLItB1UnxDP8NtyACkMCA&s" alt=""></div>
+</div>
+
+
+          </div>
+        </div>
+      </div>
+    </sub-section>
+  </section-1>
 </template>
