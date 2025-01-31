5397a1eae88bf48b566c6b5788f20035fb957a98
landryPrct <landryhabonimana@outlook.com>
2025-01-31 17:52:28 +0200

    Update main.js

--- tuuziane_front/src/main.js
+++ tuuziane_front/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
-import './node_modules/bootstrap/dist/css/bootstrap.css'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+// import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '../node_modules/boxicons/css/boxicons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
