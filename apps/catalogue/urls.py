613bc4dc85f19c9f98256320cff7774d79fd2709
Pierre Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-07-24 17:07:16 +0200

    Fixed pre-commit bugs

--- apps/catalogue/urls.py
+++ apps/catalogue/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='product')
+router.register(r"products", ProductViewSet, basename="product")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
