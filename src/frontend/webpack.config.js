93fc17d82522b31aa7bacf8a9a4480f3a245e0d1
Pierre Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-07-31 18:26:34 +0200

    Fixed the webpack statics error

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist/bundles/'),
     filename: '[name]-[contenthash].js',
-    publicPath: '/static/bundles/',
+    publicPath: '/ivyodufise-a7b3c/bundles/',
     clean: true,
   },
   plugins: [
