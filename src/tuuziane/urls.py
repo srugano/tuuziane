3b1517d6b9052c9594922a0b6906a610f64bb9ba
Pierre Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-08-06 08:28:43 +0200

    Fixing tests error for mypy

--- src/tuuziane/urls.py
+++ src/tuuziane/urls.py
@@ -26,5 +26,5 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    urlpatterns += staticfiles_urlpatterns()  # type: ignore
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
